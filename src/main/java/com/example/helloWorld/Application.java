910d792b7b2a480cad9b70d12e83b5d8cac4898b
pengyao <yao.peng@msxf.com>
2017-03-01 10:38:19 +0800

    hello world

---
+++ src/main/java/com/example/helloWorld/Application.java
@@ -0,0 +1,14 @@
+package com.example.helloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yao.peng on 2017/2/28.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
